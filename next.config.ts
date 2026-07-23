98b3b18c2dc81d8ad78a2ff583e49d70151918c3
thanhtong223 <thanhtv.svc@gmail.com>
2026-07-23 15:40:06 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* Hosted at the root of convexcam.thanh-tong.com. */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
